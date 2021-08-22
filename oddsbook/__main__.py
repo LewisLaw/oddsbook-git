6e4f934b0d71fd7198360a2f7317ec0451b8a921
Lewis Law <lewislaw@gmail.com>
2021-08-22 17:14:13 +0800

    Added repeating running for given interval.

--- oddsbook/__main__.py
+++ oddsbook/__main__.py
@@ -1,18 +1,30 @@
-import sched
 import time as timer
 from oddsbook import booker
 from oddsbook import hkjc_scraper
 
-def run():
+def run(delay: int = 3):
 
     scrapers = (hkjc_scraper.scrap_homedrawaway, hkjc_scraper.scrap_handicap)
     
     for s in scrapers:
-        odds = s()
+        odds = s(delay)
         booker.populatebook(odds)
 
-def main():
-    run()
+import argparse
+
+parser = argparse.ArgumentParser(description=r"Scrapping Odds infomation and populating to OddsBook.xlsx")
+parser.add_argument('--interval', default=0, help=r"Minutes of interval for updating odds after each run.")
+parser.add_argument('--delay', default=3, help=r"Seconds of delay on webscraping.")
+argv = parser.parse_args()
 
 if __name__ == "__main__":
-    main()
+    
+    interval = int(argv.interval)
+    delay = int(argv.delay)
+
+    if interval <= 0:
+        run(delay)
+    else:
+        while True:
+            run(delay)
+            timer.sleep(interval * 60)
